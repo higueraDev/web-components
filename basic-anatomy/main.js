2a98b8819b7226de53ecc17a0bdbc64ed8936984
David Higuera <contact@higueradev.com>
2024-02-23 13:55:35 -0600

    add: component skeleton reference

--- basic-anatomy/main.js
+++ basic-anatomy/main.js
@@ -1,2 +1,3 @@
+import { ComponentSkeleton } from "./components/ComponentSkeleton/ComponentSkeleton.js";
 import { MyComponent } from "./components/MyComponent/MyComponent.js";
 import { PollComponent } from "./components/PollComponent/PollComponent.js";
