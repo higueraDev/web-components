8b425360fc01c06faa84122ce2aed910ae70f8a2
David Higuera <contact@higueradev.com>
2024-03-16 14:05:16 -0600

    feat(star-rating): add selection handler
    - add feat to change value programmatically
    - add feat to change value by user click event

--- examples/components/StarRating/StarRating.component.js
+++ examples/components/StarRating/StarRating.component.js
@@ -20,29 +20,63 @@ class StarRating extends HTMLElement {
 	}
 
 	connectedCallback() {
+		const listener = (event) => {
+			if (this._disabled) return;
+			this.value = event.target.getAttribute("data-value");
+		};
+
 		getFiles.then((template) => {
 			this._$root.appendChild(template.content.cloneNode(true));
+			this._$container = this._$root.querySelector(".container");
+			this._$container.addEventListener("click", listener);
 		});
+
 		this._disabled = this.getAttribute("disabled") !== null;
+		this._value = this.getAttribute("value") || 0;
+		this._render();
 	}
 
 	static get observedAttributes() {
 		return ["disabled"];
 	}
 
+	set value(value) {
+		if (this._disabled) return;
+		this._value = value;
+		this._render();
+	}
+
+	get value() {
+		return this._value;
+	}
+
 	attributeChangedCallback(attr, oldValue, newValue) {
 		const attributes = {
 			disabled: () => {
-				this._disabled = newValue === "true";
+				this._disabled = newValue !== null;
 			},
 		};
 
 		if (oldValue !== newValue) return attributes[attr]();
 	}
 
-	render() {}
+	_render() {
+		if (this._$container === null) return;
 
-	disconnectedCallback() {}
+		this._$container.querySelectorAll("span").forEach((star) => {
+			const isSelectedStar =
+				this.value == star.getAttribute("data-value");
+			const isAlreadySelected = star.classList.contains("selected");
+
+			if (!isSelectedStar || isAlreadySelected)
+				star.classList.remove("selected");
+			else star.classList.add("selected");
+		});
+	}
+
+	disconnectedCallback() {
+		this._$container.removeEventListener("click", listener);
+	}
 }
 
 window.customElements.define("star-rating", StarRating);
