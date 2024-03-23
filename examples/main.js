dac4ba6ca323f5e4d5d9fedeebc4542cbb520395
David Higuera <contact@higueradev.com>
2024-03-23 13:17:11 -0600

    feat(side-menu): add slot support

--- examples/main.js
+++ examples/main.js
@@ -1,5 +1,5 @@
-import { SideMenu } from "./components/SideMenu/SideMenu.component.js";
-import { StarRating } from "./components/StarRating/StarRating.component.js";
+export { SideMenu } from "./components/SideMenu/SideMenu.component.js";
+export { StarRating } from "./components/StarRating/StarRating.component.js";
 
 const $starRating = document.querySelector("star-rating");
 
